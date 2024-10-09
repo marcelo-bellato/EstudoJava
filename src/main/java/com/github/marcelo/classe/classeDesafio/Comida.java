f5e21e53812ffae22b4e80c0ce0fe8167d9a9db6
MarceloBellato <bellato.consultor@gmail.com>
2024-10-08 23:57:35 -0300

    inclusao exercicios arrays e classes

---
+++ src/main/java/com/github/marcelo/classe/classeDesafio/Comida.java
@@ -0,0 +1,12 @@
+package com.github.marcelo.classe.classeDesafio;
+
+public class Comida {
+
+	String nome;
+	double peso;
+	
+	Comida(String nome, double peso) {
+		this.nome = nome;
+		this.peso = peso;
+	}
+}
